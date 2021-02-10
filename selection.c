fe9b32731e3ff3567a2952572de8c9720b187d60
Tiago Seabra <tlgs@users.noreply.github.com>
2021-02-10 00:35:40 +0000

    refactor: clearer variable naming

--- selection.c
+++ selection.c
@@ -2,14 +2,14 @@
 
 void selection_sort(int n, int arr[n]) {
   for (int i = 0; i < n - 1; i++) {
-    int m = i;
+    int min_idx = i;
     for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[m]) {
-        m = j;
+      if (arr[j] < arr[min_idx]) {
+        min_idx = j;
       }
     }
     int tmp = arr[i];
-    arr[i] = arr[m];
-    arr[m] = tmp;
+    arr[i] = arr[min_idx];
+    arr[min_idx] = tmp;
   }
 }
