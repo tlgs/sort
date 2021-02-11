9d2a52cf5b393b8f3caabd8a63af620fabcd7cf7
Tiago Seabra <tlgs@users.noreply.github.com>
2021-02-11 19:24:57 +0000

    style: newline between function declarations

--- sort.h
+++ sort.h
@@ -4,12 +4,19 @@
 #define SORT_ALG_N 8
 
 void bubble_sort(int n, int arr[n]);
+
 void cocktail_sort(int n, int arr[n]);
+
 void comb_sort(int n, int arr[n]);
+
 void insertion_sort(int n, int arr[n]);
+
 void merge_sort(int n, int arr[n]);
+
 void quick_sort(int n, int arr[n]);
+
 void selection_sort(int n, int arr[n]);
+
 void shell_sort(int n, int arr[n]);
 
 #endif
