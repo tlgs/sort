c8aca58767d58d9107103928ccdd469ff02ed799
Tiago Seabra <tlgs@users.noreply.github.com>
2021-02-11 03:06:08 +0000

    test: multiple runs; input data generated for each

--- test.c
+++ test.c
@@ -6,7 +6,9 @@
 
 #include "sort.h"
 
-#define ARR_SIZE 32768
+#define RUNS 10
+#define ARR_SIZE 10000
+#define RANGE_PCT 20
 
 typedef void sort_func(int n, int arr[n]);
 
@@ -21,8 +23,8 @@ int compare_int(void const *a, void const *b) {
   return (*A > *B) - (*A < *B);
 }
 
-int check(int sorted[ARR_SIZE], int arr[ARR_SIZE]) {
-  for (int i = 0; i < ARR_SIZE; i++) {
+int check(int n, int sorted[n], int arr[n]) {
+  for (int i = 0; i < n; i++) {
     if (arr[i] != sorted[i]) {
       return 0;
     }
@@ -33,18 +35,10 @@ int check(int sorted[ARR_SIZE], int arr[ARR_SIZE]) {
 int main(void) {
   srand(time(NULL));
 
-  int r[ARR_SIZE] = {0};
-  int s[ARR_SIZE] = {0};
-  for (int i = 0; i < ARR_SIZE; i++) {
-    r[i] = rand();
-    s[i] = r[i];
-  }
-  qsort(s, ARR_SIZE, sizeof(int), compare_int);
-
-  int a[ARR_SIZE] = {0};
   struct sort_algo algo[] = {
       {.name = "bubble", .f = bubble_sort},
       {.name = "cocktail", .f = cocktail_sort},
+      {.name = "comb", .f = comb_sort},
       {.name = "insertion", .f = insertion_sort},
       {.name = "merge", .f = merge_sort},
       {.name = "quick", .f = quick_sort},
@@ -52,23 +46,50 @@ int main(void) {
       {.name = "shell", .f = shell_sort},
   };
 
-  bool has_failed = false;
+  bool any_failed = false;
   for (int i = 0; i < SORT_ALG_N; i++) {
-    memcpy(a, r, sizeof(int) * ARR_SIZE);
+    bool failed = false;
+    for (int j = 0; j < RUNS && !failed; j++) {
+      int n = ARR_SIZE + (rand() % ((2 * ((ARR_SIZE * RANGE_PCT) / 100) + 1)) -
+                          (ARR_SIZE * RANGE_PCT) / 100);
 
-    algo[i].f(ARR_SIZE, a);
+      int *r = malloc(sizeof(int) * n);
+      if (!r) {
+        return EXIT_FAILURE;
+      }
+
+      int *s = malloc(sizeof(int) * n);
+      if (!s) {
+        return EXIT_FAILURE;
+      }
+
+      for (int k = 0; k < n; k++) {
+        r[k] = rand();
+        s[k] = r[k];
+      }
+      qsort(s, n, sizeof(int), compare_int);
+
+      int *a = malloc(sizeof(int) * n);
+      if (!a) {
+        return EXIT_FAILURE;
+      }
+
+      memcpy(a, r, sizeof(int) * n);
+      algo[i].f(n, a);
+      failed = !check(n, s, a);
+
+      free(r);
+      free(s);
+      free(a);
+    }
+    any_failed = (any_failed || failed);
 
     char res[5];
-    if (check(s, a)) {
-      strcpy(res, "PASS");
-    } else {
-      has_failed = true;
-      strcpy(res, "FAIL");
-    }
+    strcpy(res, failed ? "FAIL" : "PASS");
     int spaces_n = 10 - strlen(algo[i].name);
     printf("[%d/%d] %s:%*c%s\n", i + 1, SORT_ALG_N, algo[i].name, spaces_n, ' ',
            res);
   }
 
-  return has_failed ? EXIT_FAILURE : EXIT_SUCCESS;
+  return any_failed ? EXIT_FAILURE : EXIT_SUCCESS;
 }
