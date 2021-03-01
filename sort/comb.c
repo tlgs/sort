cd57f3ff13d3a54e43f000759f9bd42f351dec90
Tiago Seabra <tlgs@users.noreply.github.com>
2021-03-01 20:12:01 +0000

    refactor: Combsort11

--- sort/comb.c
+++ sort/comb.c
@@ -3,14 +3,23 @@
 
 #include "sort.h"
 
-#define K 1.3
-
+// comb sort is a variation of bubble sort that attempts to "kill turtles"
+// more effectively than the cocktail sort variant.
+// the idea of comparing elements a great distance apart allow for "turtles"
+// to move great distances quickly; by the time the gap approaches 1, most
+// "turtles" have been dealt with and bubble sort is rather efficient.
+//
+// the shrinking factor and prefered ending sequence (Combsort11) are determined
+// empirically as described by Stepehen Lacy and Richard Box in:
+// <http://cs.clackamas.cc.or.us/molatore/cs260Spr03/combsort.htm>
 void comb_sort(size_t n, int32_t arr[n]) {
   size_t gap = n;
   bool swapped;
   do {
-    gap /= K;
-    if (gap < 1) {
+    gap /= 1.3;
+    if (gap == 9 || gap == 10) {
+      gap = 11;
+    } else if (gap < 1) {
       gap = 1;
     }
 
