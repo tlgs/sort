cd015fbae961898e1003fa48e6ed36f10362cf4d
Tiago Seabra <tlgs@users.noreply.github.com>
2021-03-02 23:42:34 +0000

    docs: cocktail algorithm description

--- sort/cocktail.c
+++ sort/cocktail.c
@@ -2,9 +2,12 @@
 
 #include "sort.h"
 
-// cocktail sort is a variation of bubble sort that operates in both directions;
-// this hopes to "kill turtles", i.e. move small elements to the beginning of
-// the list.
+// cocktail sort is a variation of bubble sort that operates in both directions.
+// with bubble sort, small elements at the end of the list take a long time to
+// arrive at their correct position (they change at most 1 position per pass);
+// cocktail sort aims to mitigate this "turtle" problem by alternating
+// directions
+//
 // uses the "last swap" optimization to reduce comparison count
 void cocktail_sort(size_t n, int32_t arr[n]) {
   size_t start = 0;
@@ -18,7 +21,7 @@ void cocktail_sort(size_t n, int32_t arr[n]) {
     }
     n = last_swap;
 
-    for (size_t i = n; i > start + 1; i--) {
+    for (size_t i = n; i >= start + 2; i--) {
       if (arr[i - 1] < arr[i - 2]) {
         swap(&arr[i - 2], &arr[i - 1]);
         last_swap = i - 2;
